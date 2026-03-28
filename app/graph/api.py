bd8d26ac4f193b278ca2a6eeb7b5ead9cc6a86c7
Rafal Tekielski <rafaltekielski@gmail.com>
2026-03-28 22:51:10 +0100

    Add endpoint to get current graph

--- app/graph/api.py
+++ app/graph/api.py
@@ -1,10 +1,19 @@
 import logging
 
 from fastapi import APIRouter
+
+from app.graph.model.graph import GraphDetails
+from app.graph.repository.graph_repository import GraphRepository
 from app.kb.knowledge_base_service import knowledge_base_service
+from app.graph.service.graph_service import GraphService
 
 logger = logging.getLogger(__name__)
 router = APIRouter()
+
+
+@router.get("/graphs", status_code=200)  ## response_model=GraphDetails | None
+async def get_latest_graph():
+    return GraphService(GraphRepository()).get_latest_graph()
 
 
 @router.delete("/graphs/clean", status_code=204)
